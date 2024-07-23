5d413216fb59f1b4f20fd791544beeb646793e17
SJAlanA <sahaijordialan@gmail.com>
2024-07-23 19:56:38 +0530

    Created all the files - No implementation of algorithm yet

---
+++ Source Files/verifyID.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argv, char *argc) {
+    cout<<"You have entered " << argv << " inputs";
+    cout<<"ID: "<<argv[1];
+
+    return 0;
+}
